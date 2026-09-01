4cebdb18e252566cb52166cf1d511fb8de45b29c
SKCRAZY <alitiji94@gmail.com>
2026-09-01 12:43:35 +0100

    Add Centre Les Profs metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,3 @@
-import "./globals.css"; export default function Layout({children}:{children:React.ReactNode}){return <html lang="fr"><body>{children}</body></html>}
+import "./globals.css";
+export const metadata={title:"Centre Les Profs",description:"Centre de soutien scolaire"};
+export default function Layout({children}:{children:React.ReactNode}){return <html lang="fr"><body>{children}</body></html>}
