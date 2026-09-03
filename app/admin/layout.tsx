f9742772ff7e29d914157e14d21af06abd79110e
SKCRAZY <alitiji94@gmail.com>
2026-09-03 18:30:15 +0100

    Link New Seance component to admin page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,7 @@
 import AdminMenu from './AdminMenu';
 import PhoneDuplicateGuard from './PhoneDuplicateGuard';
+import NewSeance from './NewSeance';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}<AdminMenu /><PhoneDuplicateGuard /></>;
+  return <>{children}<AdminMenu /><PhoneDuplicateGuard /><NewSeance /></>;
 }
