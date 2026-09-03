3fb7c5def6c93f77632673ff05b93246425af5bc
SKCRAZY <alitiji94@gmail.com>
2026-09-03 17:17:57 +0100

    Enable duplicate phone number protection

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,6 @@
 import AdminMenu from './AdminMenu';
+import PhoneDuplicateGuard from './PhoneDuplicateGuard';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}<AdminMenu /></>;
+  return <>{children}<AdminMenu /><PhoneDuplicateGuard /></>;
 }
